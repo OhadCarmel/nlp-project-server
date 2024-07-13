d34005f1ab6402e2cf211f0b64a76ce4303d74a7
ocarmel1 <ocarmel@gmail.com>
2024-07-13 12:40:22 +0300

    .

--- index.js
+++ index.js
@@ -69,7 +69,9 @@ app.use(cookieParser());
 
 const corsOptions = {
   origin: `${baseUrl.deployedClient}`,
-  credentials: true,
+  // credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+
 };
 app.use(cors(corsOptions));
 
