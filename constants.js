c157ddd0177df5ddb0ae468be8cbdc24220ab170
ocarmel1 <ocarmel@gmail.com>
2024-07-13 20:44:22 +0300

    removed https

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 const baseUrl = {
     client: "http://localhost:3000",
     server: "http://localhost:3080",
-    deployedClient: "https://ohadcarmel.github.io",
+    deployedClient: "ohadcarmel.github.io",
 };
 
 module.exports = { baseUrl };
