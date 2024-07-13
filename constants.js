0bcbe2e571bee33f4bcd8af6f5e4800cfbd07f69
ocarmel1 <ocarmel@gmail.com>
2024-07-13 20:47:33 +0300

    .

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 const baseUrl = {
     client: "http://localhost:3000",
     server: "http://localhost:3080",
-    deployedClient: "ohadcarmel.github.io",
+    deployedClient: "https://ohadcarmel.github.io",
 };
 
 module.exports = { baseUrl };
